7dcaa5dde2ffcaaa6b3a83d2530bc6d37d4ef12d
hammerkid <hammerkid123@yandex.ru>
2016-08-31 16:19:14 +0300

    question about yeild return

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-d = dict(a=1, b=2, c=3)
+class Car():
+    def __init__(self):
+        self.tank =20
+        self.kms = 0
 
-for key in d:
-    print (key, "=", d[key])
+    def ride(self):
+        while self.kms !=199:
+            self.kms +=1
+            self.tank -= 1
+            yield self.tank
 
-for keys in d.keys():
-    print keys,"=", d[keys]
+    def refill(self):
+        if self.tank == 0:
+            print ('Refilling')
+            self.tank = 100
+            return self.tank
+
+car = Car()
+car.ride()
+
+print car.__dict__
